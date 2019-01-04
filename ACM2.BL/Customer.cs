1003fa24bbd4ac68f4d196cd224fc0bc84a68d32
unknown <key.man@gmx.com>
2019-01-05 02:47:39 +0800

    Adding Encapsulation class member

--- ACM2.BL/Customer.cs
+++ ACM2.BL/Customer.cs
@@ -4,5 +4,28 @@ namespace ACM2
 {
     public class Customer
     {
+        //In case need any code in get/set block
+        //Full property syntax
+        private string _lastName { get; set; }
+
+        public string LastName 
+        { 
+            get=> _lastName;
+            set=> _lastName = value;
+        }
+
+        //In case dont need any logic within getter/setter block
+        public string FirstName { get; set; } //auto implemented property
+        public string EmailAddress { get; set; }
+
+        //Database is going to set the id
+        public int CustomerId { get; private set; }
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+
+        public string FullName 
+        { 
+            get=> LastName +","+ FirstName;
+        }
     }
 }
